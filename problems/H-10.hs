54fcb023da3a45734a90e8b7be34973ceac2805a
Evan Turner <evturn@gmail.com>
2017-01-22 21:14:16 -0500

    Add type signature to h-10

---
+++ problems/H-10.hs
@@ -0,0 +1,4 @@
+-- 10 Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+-- Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+
+encode :: Eq a => [a] -> [(Int, a)]
