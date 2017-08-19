b93c4ae6afee4192b918fa2de728e5c6a1156b77
Evan Turner <evturn@gmail.com>
2017-08-19 11:12:41 -0400

    Complete problem 11

---
+++ problems/H-11.hs
@@ -0,0 +1,17 @@
+-- Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+
+import Data.List
+
+data EncodedItem a =
+    Single a
+  | Multiple Int a
+  deriving (Eq, Show)
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = [(length x, head x) | x <- group xs]
+
+encodeModified :: Eq a => [a] -> [EncodedItem a] 
+encodeModified = map doIt . encode
+  where
+    doIt (1, x) = Single x
+    doIt (n, x) = Multiple n x
